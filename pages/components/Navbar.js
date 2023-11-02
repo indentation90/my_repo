50262499942167d9a190c244384103946b1584d4
indentation90 <basyariababil@gmail.com>
2023-11-02 10:58:24 +0700

    new

---
+++ pages/components/Navbar.js
@@ -0,0 +1,24 @@
+const Navbar = () => {
+    return (
+    <nav className="navbar">
+        <ul>
+            <li>
+                <a href="">Home</a>
+            </li>
+            <li>
+                <a href="">About</a>
+            </li>
+            <li>
+                <a href="">portfolio</a>
+            </li>
+                <li>
+                <a href="">Contact</a>
+            </li>
+            
+        </ul>
+    </nav>
+    );
+
+};
+
+export default Navbar;
